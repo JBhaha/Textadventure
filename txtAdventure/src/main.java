ce8fe99f5c0022f3c5a2deafcdecd3b7bfa1f536
Hermann Witte <witteh@bzz.ch>
2021-09-16 17:10:17 +0200

    main Klasse erstellt

---
+++ txtAdventure/src/main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/**
+ * Small textadventure
+ *
+ * @author Philip, Hermann & Jakub
+ * @since 16.09.2021
+ * @version 1.0
+ */
+public class main {
+    static Scanner scanner;
+    public main(){
+        scanner = new Scanner(System.in);
+    }
+    public static void main(String[] args) {
+        main Program = new main();
+        Program.run();
+    }
+
+    public void run(){
+
+    }
+}
