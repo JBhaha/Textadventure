942cc4ec96763af0031572df055cf495913c4626
Philip Jovanovic <jovanovicp@bzz.ch>
2021-09-17 07:38:43 +0200

    changed some attribute names, made attack method added maxhealth

--- txtAdventure/src/Adventurer.java
+++ txtAdventure/src/Adventurer.java
@@ -45,7 +45,7 @@ public class Adventurer {
         this.name = name;
     }
 
-    public void setHealth(double health) {
+    public void sethp(double health) {
         this.hp = hp;
     }
 
@@ -75,6 +75,14 @@ public class Adventurer {
 
     public void setLevel(int level) {
         this.level = level;
+    }
+
+    public double getMaxhp() {
+        return maxhp;
+    }
+
+    public void setMaxhp(double maxhp) {
+        this.maxhp = maxhp;
     }
     // Methods
 
