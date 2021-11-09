da88ebae9cf2c6f8b0052b756590feddb450fdac
JBhaha <jakus.korona@gmail.com>
2021-11-09 14:55:29 +0100

    Added a bunch of places and changed the description of the class ;)

--- txtAdventure/src/Game.java
+++ txtAdventure/src/Game.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 /**
- * Class for places
+ * Game logic (This is the good stuff)
  **/
 public class Game {
     static Scanner scanner;
@@ -25,9 +25,65 @@ public class Game {
     private final String pathText = "You're standing on the Path. At the end of the Path, there is a clearing.";
     private final Place path = new Place(5, "Path", pathText);
 
+    //attributes for the clearing
     private final String clearingText = "You get to the clearing. The warm sunshine is blinding you. \n" +
             "As you're trying to glance at the sun, you notice a weird-looking man next to you.";
     private final Place clearing = new Place(5, "Clearing", clearingText);
+
+    //attributes for the branch
+    private final String branchText = "You're standing on a Branch. There are three paths.";
+    private final Place branch = new Place(0, "Branch", branchText);
+
+    //attributes for the cabin
+    private final String cabinText = "You're in a cabin. It's cold and spooky.";
+    private final Place cabin = new Place(5, "Cabin", cabinText);
+
+    //attributes for the camp
+    private final String campText = "This is a camp";
+    private final Place camp = new Place(5, "Camp", campText);
+
+    //attributes for the lake
+    private final String lakeText = "This is a lake";
+    private final Place lake = new Place(0, "Lake", lakeText);
+
+    //attributes for the river
+    private final String riverText = "This is a river";
+    private final Place river = new Place(0, "River", riverText);
+
+    //attributes for the dunes
+    private final String dunesText = "Those are some huge dunes";
+    private final Place dunes = new Place(0, "Dunes", dunesText);
+
+    //TODO: Add a place next to the dunes
+
+    //attributes for the desert
+    private final String desertText = "Sand everywhere! I guess we're in a desert!";
+    private final Place desert = new Place(0, "Desert", desertText);
+
+    //attributes for the island
+    private final String islandText = "This is an island lol";
+    private final Place island = new Place(0, "Island", islandText);
+
+    //attributes for the town
+    private final String townText = "You've reached the town";
+    private final Place town = new Place(0, "Town", townText);
+
+    //TODO: 4 more places between the desert and the town
+
+    //Last places
+
+    //attributes for the mountain
+    private final String mountainText = "Woah! You can see everything from the top of this mountain!";
+    private final Place mountain = new Place(0, "Mountain", mountainText);
+
+    //attributes for the temple
+    private final String templeText = "It's so dark in here...this temple is really spooky";
+    private final Place temple = new Place(0, "temple", templeText);
+
+    //attributes for the graveyard
+    private final String graveyardText = "Soooo...Graveyard.";
+    private final Place graveyard = new Place(0, "Graveyard", graveyardText);
+
     //attributes for the adventurer
     Adventurer adventurer = new Adventurer("Alex", 40, 50, 1);
     Backpack defaultBackpack = new Backpack(20);
@@ -48,12 +104,31 @@ public class Game {
 
         //objects which are in the garden at the beginning
         garden.addObjectsToPlace(shovel);
+
         house.addAccessiblePlaces(garden);
+
         garden.addAccessiblePlaces(house);
         garden.addAccessiblePlaces(path);
+
         path.addAccessiblePlaces(garden);
         path.addAccessiblePlaces(clearing);
+
         clearing.addAccessiblePlaces(path);
+        clearing.addAccessiblePlaces(branch);
+
+        branch.addAccessiblePlaces(clearing);
+        branch.addAccessiblePlaces(cabin);
+        branch.addAccessiblePlaces(camp);
+
+        cabin.addAccessiblePlaces(branch);
+
+        camp.addAccessiblePlaces(branch);
+        camp.addAccessiblePlaces(lake);
+
+        lake.addAccessiblePlaces(camp);
+        lake.addAccessiblePlaces(river);
+
+        river.addAccessiblePlaces(lake);
 
         //Creatures Arrays
         String[] wizardOptions = new String[3];
@@ -74,7 +149,7 @@ public class Game {
 
         //add creatures to place
         clearing.addCreatures(wizard);
-    }   
+    }
 
     //Start method of the Textadventure!
     public void start(){
